b1002712af26ad0c037c223f010d1963935a0d3f
ryanechternacht <ryan@echternacht.org>
2022-07-17 14:53:41 -0400

    Read files from folder

--- src/dbcopy_api/cli/generate_template.clj
+++ src/dbcopy_api/cli/generate_template.clj
@@ -1,20 +1,28 @@
 (ns dbcopy-api.cli.generate-template
   (:require [clj-yaml.core :as yaml]
+            [clojure.java.io :as io]
             [dbcopy-api.dependencies :as dep]))
+
+(defn- read-files-from-folder
+  "Given a folder slurps and `read-strings` every file in it"
+  [folder]
+  (->> (io/file folder)
+       file-seq
+       (filter #(.isFile %))
+       (map slurp)
+       (map read-string)))
 
 ;; TODO is there a prettier printing form for yaml?
 ;;   - ideally, we'd put newlines between top level elems
 ;;   - ideally, we'd print blank for nil (now it prints null, so we use "")
 (defn generate-template [{template-file :file}]
-  (let [;; TODO pull from folder (need internet)
-        db-files [".dbcopy/dbs/yardstick.edn" ".dbcopy/dbs/ys.edn"]
-        tables (reduce (fn [m f]
-                         (let [spec (read-string (slurp f))]
-                           (assoc m
-                                  (:nickname spec)
-                                  (map :table (dep/get-all-tables spec)))))
+  (let [db-specs (read-files-from-folder ".dbcopy/dbs")
+        tables (reduce (fn [m spec]
+                         (assoc m
+                                (:nickname spec)
+                                (map :table (dep/get-all-tables spec))))
                        {}
-                       db-files)]
+                       db-specs)]
     (->> {:version "v0.1"
           :root-table {:db ""
                        :schema ""
@@ -27,4 +35,3 @@
          (#(yaml/generate-string % :dumper-options {:flow-style :block}))
          (spit template-file))
     (println "Generated template at" template-file)))
-
